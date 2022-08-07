774f6f54a23990359f54d81b37c5dfc56e913cc8
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-08-07 10:40:41 +0200

    Model implemented

--- MVVMProject/Model/HelloWorldModel.cs
+++ MVVMProject/Model/HelloWorldModel.cs
@@ -8,5 +8,41 @@ namespace MVVMProject.Model
 {
     internal class HelloWorldModel
     {
+        private List<string> repositoryData;
+        public string ImportantInfo
+        {
+            get
+            {
+                return ConcatenateData(repositoryData);
+            }
+        }
+
+        public HelloWorldModel()
+        {
+            repositoryData = GetData();
+        }
+
+        /// <summary>
+        /// Simulates data retrival from a reposittory
+        /// </summary>
+        /// <returns> List of strings </returns>
+        private List<string> GetData()
+        {
+            repositoryData = new List<string>()
+            {
+                "Hello",
+                "world"
+            };
+            return repositoryData;
+        }
+        /// <summary>
+        /// Concetenate the information from the list into a fully formed sentence.
+        /// </summary>
+        /// <returns> A string </returns>
+        private string ConcatenateData(List<string> dataList)
+        {
+            string importantInfo = dataList.ElementAt(0) + ", " + dataList.ElementAt(1) + "!";
+            return importantInfo;
+        }
     }
 }
