3a9467e9c3c8f9e072c2e1ccfb9f378d9c28f963
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-08-07 10:24:53 +0200

    ViewModel implemented

--- MVVMProject/ViewModel/HelloWorldViewModel.cs
+++ MVVMProject/ViewModel/HelloWorldViewModel.cs
@@ -1,14 +1,48 @@
-﻿using System;
+﻿using MVVMProject.Model;
+using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace MVVMProject.ViewModel
 {
-    internal class HelloWorldViewModel
+    // Implements INottifyPropertyChanged interface to support bindings
+    internal class HelloWorldViewModel : INotifyPropertyChanged
     {
+        private string helloString;
 
-        // Implements INottifyPropertyChanged interface to support bindings
+        public event PropertyChangedEventHandler? PropertyChanged;
+
+        public string HelloString
+        {
+            get
+            {
+                return helloString;
+            }
+            set
+            {
+                helloString = value;
+                OnPropertyChanged();
+            }
+        }
+
+        // <summary>
+        /// Raises OnPropertychangedEvent when property changes
+        /// </summary>
+        /// <param name="name"> String represenging the property name </param>
+        protected void OnPropertyChanged([CallerMemberName] string name = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+
+        public HelloWorldViewModel()
+        {
+            HelloWorldModel helloWorldModel = new HelloWorldModel();
+            helloString = helloWorldModel.ImportantInfo;
+        }
+
     }
 }
