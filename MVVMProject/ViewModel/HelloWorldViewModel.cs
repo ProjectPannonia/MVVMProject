9d4a5feda38ed9ed5dfbafd8aa9df0a8af0dcbae
Adam Letenyei <35657485+ProjectPannonia@users.noreply.github.com>
2022-08-07 10:12:44 +0200

    Project basic skeleton created

---
+++ MVVMProject/ViewModel/HelloWorldViewModel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MVVMProject.ViewModel
+{
+    internal class HelloWorldViewModel
+    {
+
+        // Implements INottifyPropertyChanged interface to support bindings
+    }
+}
